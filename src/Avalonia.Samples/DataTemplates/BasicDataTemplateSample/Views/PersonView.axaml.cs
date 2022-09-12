90428cf58f11e3801bdb8f34b8301084a9ad2e5f
timunie <tim.uniewski@gmail.com>
2022-09-12 19:18:46 +0200

    Add UserControl usage demo

---
+++ src/Avalonia.Samples/DataTemplates/BasicDataTemplateSample/Views/PersonView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace BasicDataTemplateSample.Views
+{
+    public partial class PersonView : UserControl
+    {
+        public PersonView()
+        {
+            InitializeComponent();
+        }
+    }
+}
