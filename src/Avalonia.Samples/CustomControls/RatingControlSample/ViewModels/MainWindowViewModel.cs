7cadcd4b40098cab4e135464eb6d97669f9931a1
timunie <tim.uniewski@gmail.com>
2022-11-28 20:49:04 +0100

    WIP: Adding Rating Sample

---
+++ src/Avalonia.Samples/CustomControls/RatingControlSample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RatingControlSample.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
