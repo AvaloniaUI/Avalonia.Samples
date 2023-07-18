118372204ed285642586c0b456a671d366a71bf8
timunie <tim.uniewski@gmail.com>
2023-07-18 21:46:06 +0200

    Upgrade ViewLocator-Sample

--- src/Avalonia.Samples/Routing/BasicViewLocatorSample/ViewLocator.cs
+++ src/Avalonia.Samples/Routing/BasicViewLocatorSample/ViewLocator.cs
@@ -7,8 +7,13 @@ namespace BasicViewLocatorSample
 {
     public class ViewLocator : IDataTemplate
     {
-        public IControl Build(object data)
+        public Control Build(object? data)
         {
+            if (data is null)
+            {
+                return new TextBlock { Text = "data was null" };
+            }
+            
             var name = data.GetType().FullName!.Replace("ViewModel", "View");
             var type = Type.GetType(name);
 
@@ -22,7 +27,7 @@ namespace BasicViewLocatorSample
             }
         }
 
-        public bool Match(object data)
+        public bool Match(object? data)
         {
             return data is ViewModelBase;
         }
