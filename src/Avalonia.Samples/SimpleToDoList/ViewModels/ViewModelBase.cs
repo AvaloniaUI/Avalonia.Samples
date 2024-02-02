0526718537d1706ad78a02c5d03802d1fea43881
timunie <tim.uniewski@gmail.com>
2024-02-02 22:59:58 +0100

    Init work for SimpleToDo-App
    This sample will not cover Dialogs nor DataPersistence. That should be done in a more complex App following later

---
+++ src/Avalonia.Samples/SimpleToDoList/ViewModels/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SimpleToDoList.ViewModels;
+
+/*   NOTE:
+ *
+ *   Please mind that this samples uses the CommunityToolkit.Mvvm package for the ViewModel. Feel free to use any other
+ *   MVVM-Framework (like ReactiveUI or Prsim) that suits your needs best.
+ */
+
+/// <summary>
+/// A base class for all of our ViewModels.
+/// </summary>
+public class ViewModelBase : ObservableObject
+{
+}
