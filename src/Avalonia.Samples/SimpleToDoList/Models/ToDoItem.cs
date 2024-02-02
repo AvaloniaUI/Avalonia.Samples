81644f114dc413ae1f4bb5cc980f153bce9df8c1
timunie <tim.uniewski@gmail.com>
2024-02-02 22:59:58 +0100

    Init work for SimpleToDo-App
    This sample will not cover Dialogs nor DataPersistence. That should be done in a more complex App following later

---
+++ src/Avalonia.Samples/SimpleToDoList/Models/ToDoItem.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SimpleToDoList.Models;
+
+/*   NOTE:
+ * 
+ *   Please Mind that this samples uses the CommunityToolkit.Mvvm package for the Model. Feel free to use any other
+ *   MVVM-Framework (like ReactiveUI or Prsim) that suits your needs best.
+ */
+
+/// <summary>
+/// This is our Model for a simple ToDoItem. 
+/// </summary>
+public partial class ToDoItem : ObservableObject 
+{
+    /// <summary>
+    /// Gets or sets the checked status of each item
+    /// </summary>
+    [ObservableProperty] 
+    private bool _IsChecked;
+
+    /// <summary>
+    /// Gets or sets the content of the to-do item
+    /// </summary>
+    /// <returns></returns>
+    [ObservableProperty] 
+    private string? _Content;
+}
