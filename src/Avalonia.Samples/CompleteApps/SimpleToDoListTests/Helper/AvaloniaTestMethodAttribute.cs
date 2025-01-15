124f279bb2a90095136afcf044dd84cb85beba11
Joe Care <Joe_care_99@yahoo.com>
2025-01-15 09:17:24 +0100

    SimpleToDoListTests

--- src/Avalonia.Samples/CompleteApps/SimpleToDoListTests/Helper/AvaloniaTestMethodAttribute.cs
+++ src/Avalonia.Samples/CompleteApps/SimpleToDoListTests/Helper/AvaloniaTestMethodAttribute.cs
@@ -1,27 +1,34 @@
-﻿using System;
-using global::Avalonia.Headless;
-using Microsoft.VisualStudio.TestPlatform.ObjectModel.Engine.ClientProtocol;
-using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
+﻿using System.Reflection;
 
 namespace Avalonia.Headless.MSTest;
 
-//
-// Zusammenfassung:
-//     Identifies a nunit test that starts on Avalonia Dispatcher such that awaited
-//     expressions resume on the test's "main thread".
+/// <summary>
+///   <para>
+/// This Attribute identifies a TestMethod that starts on Avalonia Dispatcher such that awaited expressions resume in the test's main thread</para>
+///   <para>Class AvaloniaTestMethodAttribute.<br />This class cannot be inherited.
+/// <br /> Implements the <see cref="TestMethodAttribute" /></para>
+/// </summary>
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = false)]
 public sealed class AvaloniaTestMethodAttribute : TestMethodAttribute
 {
     public override TestResult[] Execute(ITestMethod testMethod)
     {
-        using var _session = HeadlessUnitTestSession.GetOrStartForAssembly(testMethod?.MethodInfo.DeclaringType?.Assembly);
+        var assembly = testMethod.MethodInfo.DeclaringType!.Assembly;
+        var appBuilderEntryPointType = assembly.GetCustomAttribute<AvaloniaTestApplicationAttribute>()
+            ?.AppBuilderEntryPointType;
+
+        appBuilderEntryPointType ??= typeof(Application);
+
+        using var _session = HeadlessUnitTestSession.StartNew(appBuilderEntryPointType);
         {
-            return _session.Dispatch(() => ExecuteTestMethod(testMethod), default).GetAwaiter().GetResult();
+            return _session.Dispatch(() => ExecuteTestMethod(testMethod!), default).GetAwaiter().GetResult();
         }
     }
 
-    // Unfortunately, NUnit has issues with custom synchronization contexts, which means we need to add some hacks to make it work.
-    private async Task<TestResult[]> ExecuteTestMethod(ITestMethod testMethod)
+    /// <summary>Executes the test method.</summary>
+    /// <param name="testMethod">The test method.</param>
+    /// <returns>TestResult[].</returns>
+    private static TestResult[] ExecuteTestMethod(ITestMethod testMethod)
     {
         return [testMethod.Invoke(null)];
     }
