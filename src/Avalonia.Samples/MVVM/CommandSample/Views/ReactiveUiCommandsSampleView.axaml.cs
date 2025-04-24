f87f75ffb496128bdd621418f484c7d27f178c76
timunie <tim.uniewski@gmail.com>
2025-04-24 22:20:15 +0200

    refractor Commands Sample

---
+++ src/Avalonia.Samples/MVVM/CommandSample/Views/ReactiveUiCommandsSampleView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CommandSample.Views;
+
+public partial class ReactiveUiCommandsSampleView : UserControl
+{
+    public ReactiveUiCommandsSampleView()
+    {
+        InitializeComponent();
+    }
+}
