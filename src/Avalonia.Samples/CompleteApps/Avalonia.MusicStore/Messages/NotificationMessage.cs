08ea40e50b91e3140c5bb3ff06bdf8e9e686bebb
Tim <47110241+timunie@users.noreply.github.com>
2025-06-24 06:50:54 +0200

    Some further improvements
    - Rename to MainViewModel
    - Check if an Album was added yet

---
+++ src/Avalonia.Samples/CompleteApps/Avalonia.MusicStore/Messages/NotificationMessage.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+
+namespace Avalonia.MusicStore.Messages;
+
+public class NotificationMessage 
+{
+    public NotificationMessage(string message)
+     {
+         Message = message;
+     }
+    public string Message { get; }
+}
