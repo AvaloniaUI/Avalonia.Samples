2709179f3c98398518ef6ebddd75980926dc82af
timunie <tim.uniewski@gmail.com>
2026-04-11 20:30:09 +0200

    Update MVVM samples

--- src/Avalonia.Samples/MVVM/ValueConversionSample/Program.cs
+++ src/Avalonia.Samples/MVVM/ValueConversionSample/Program.cs
@@ -18,6 +18,7 @@ namespace ValueConversionSample
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
                 .LogToTrace()
+                .WithDeveloperTools()
                 .UseReactiveUI(builder => { /* We don't need to configure anything for the samples */ });
     }
 }
