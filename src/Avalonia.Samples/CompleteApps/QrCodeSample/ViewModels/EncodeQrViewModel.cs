f54ab5feee44a5384daa3df57e7e65c4b9227ee7
timunie <tim.uniewski@gmail.com>
2026-07-18 21:22:44 +0200

    Move the sample to complete Apps.
    In the end this is more of a complete App than a drwaing sample

--- src/Avalonia.Samples/CompleteApps/QrCodeSample/ViewModels/EncodeQrViewModel.cs
+++ src/Avalonia.Samples/CompleteApps/QrCodeSample/ViewModels/EncodeQrViewModel.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.IO;
 using System.Threading.Tasks;
 using Avalonia.Media;
@@ -5,6 +6,7 @@ using Avalonia.Media.Imaging;
 using Avalonia.Platform.Storage;
 using CodeGlyphX;
 using CodeGlyphX.Rendering;
+using CodeGlyphX.Rendering.Png;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using QrCodeSample.Services;
@@ -13,20 +15,15 @@ namespace QrCodeSample.ViewModels;
 
 public partial class EncodeQrViewModel : ViewModelBase, IDialogParticipant
 {
-    [ObservableProperty]
+    [ObservableProperty] 
     public partial string? TextToEncode { get; set; }
-    
-    [ObservableProperty]
-    public partial Color ForegroundColor {get;set;}
-    
-    [ObservableProperty]
-    public partial Color BackgroundColor {get;set;}
-    
-    partial void OnTextToEncodeChanged(string? value)
-    {
-        EncodeQr();
-    }
-    
+
+    [ObservableProperty] 
+    public partial Color ForegroundColor { get; set; } = Colors.Black;
+
+    [ObservableProperty] 
+    public partial Color BackgroundColor { get; set; } = Colors.White;
+
     [ObservableProperty]
     [NotifyCanExecuteChangedFor(nameof(SaveCommand))]
     [NotifyCanExecuteChangedFor(nameof(CopyCommand))]
@@ -35,22 +32,33 @@ public partial class EncodeQrViewModel : ViewModelBase, IDialogParticipant
     private void EncodeQr()
     {
         if (TextToEncode == null) return;
-        
+
         var qrCode = QrCodeEncoder.EncodeText(TextToEncode);
 
+        var options = new QrEasyOptions()
+        {
+            Foreground = GetColorRgba32(ForegroundColor),
+            Background = GetColorRgba32(BackgroundColor),
+        };
+
+        Rgba32 GetColorRgba32(Color color)
+        {
+            return new Rgba32(color.R, color.G, color.B, color.A);
+        }
+
         using var ms = new MemoryStream();
-        qrCode.Save(OutputFormat.Png, ms);
-        
+        qrCode.Save(OutputFormat.Png, ms, options);
+
         ms.Seek(0, SeekOrigin.Begin);
         QrCodeImage = new Bitmap(ms);
     }
 
     private bool HasQrCodeImage => QrCodeImage != null;
-    
-    [RelayCommand (CanExecute = nameof(HasQrCodeImage))]
+
+    [RelayCommand(CanExecute = nameof(HasQrCodeImage))]
     private async Task SaveAsync()
     {
-        var saveFile = await this.SaveFileDialogAsync(title: "Save QR Code Image", 
+        var saveFile = await this.SaveFileDialogAsync(title: "Save QR Code Image",
             filePickerFileTypes: new[] { FilePickerFileTypes.ImagePng });
 
         if (saveFile.HasValue && QrCodeImage is not null)
@@ -60,9 +68,23 @@ public partial class EncodeQrViewModel : ViewModelBase, IDialogParticipant
         }
     }
 
-    [RelayCommand (CanExecute = nameof(HasQrCodeImage))]
+    [RelayCommand(CanExecute = nameof(HasQrCodeImage))]
     private async Task CopyAsync()
     {
         await this.TrySetImageToClipboard(QrCodeImage!);
+    }
+
+    protected override void OnPropertyChanged(PropertyChangedEventArgs e)
+    {
+        base.OnPropertyChanged(e);
+
+        switch (e.PropertyName)
+        {
+            case nameof(TextToEncode):
+            case nameof(ForegroundColor):
+            case nameof(BackgroundColor):
+                EncodeQr();
+                break;
+        }
     }
 }
