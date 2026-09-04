7fcd2d9e4649b3a73a0e6e97859ddbd3425d6f58
Kirill Golubenko <golubenkokirill@outlook.com>
2026-09-04 15:09:43 +0500

    feat(project): add avalonia project

---
+++ src/Avalonia.Samples/RestAPI/RestApiSample/ViewModels/MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace RestApiSample.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    public partial string Greeting { get; set; } = "Welcome to Avalonia!";
+}
